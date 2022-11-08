5f148d24fff0f1fbd485574d4898fc02c6d0854b
Afif Akromi <massachusetts48@gmail.com>
2022-11-08 19:43:18 +0700

    add calendar

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Inject,
+  Search,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
 
-const Employees = () => {
-  return <div>Employees</div>;
+import { employeesGrid, employeesData } from "../data/dummy";
+import { Header } from "../components";
+
+const Employess = () => {
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Employees" category="Page" />
+      <GridComponent
+        dataSource={employeesData}
+        toolbar={["Search"]}
+        allowSorting
+        allowPaging
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
-export default Employees;
+export default Employess;
